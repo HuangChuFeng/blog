6e5f9b78d2fd3ea11643ffb67eeac21240a93f6d
HuangChuFeng <qy_hcf@outlook.com>
2019-05-12 22:04:31 +0800

    fix

--- server/config/default.js
+++ server/config/default.js
@@ -1,4 +1,8 @@
-const DEV_URL = 'http://localhost:3001'; // process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://www.huangchufeng.site:3001';
+
+// 上线时修改
+// const DEV_URL = 'http://localhost';
+const DEV_URL = 'http://www.huangchufeng.site';
+
 module.exports = {
 	port: 3000,
 	secretKey: "qy",
@@ -8,5 +12,5 @@ module.exports = {
 		maxAge: '2592000000'
 	},
 	mongodb: 'mongodb://localhost:27017/myblog',
-	cors: DEV_URL,
+	cors: DEV_URL + ':3001/',
 }
--- server/controllers/util.js
+++ server/controllers/util.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-
-const DEV_URL = 'http://localhost:3000/'; //process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'http://www.huangchufeng.site:3000/';
+// 上线时修改
+// const DEV_URL = 'http://localhost';
+const DEV_URL = 'http://www.huangchufeng.site';
 module.exports = { 
-    domain: DEV_URL,
-
+    domain: DEV_URL + ':3000/',
     //删除临时图片目录下的所有图片
     deleteFolder: (path) => {
         var files = [];
